a39841c0423b3133dcd38eac5d9d9679fecf1108
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-08-20 15:22:51 -0700

    Modified the original docker-compose file, the original env file, and added a basic structure for future additions.

---
+++ app/design/frontend/Stonesoup/s3_base/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2015 Magento. all rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Stonesopup/s3_base',
+    __DIR__
+);
